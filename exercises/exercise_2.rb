02d97398fe114858bf2512444a45a9b231d093ec
DinoPranjic <dinopranjic92@gmail.com>
2022-06-27 23:45:20 +0000

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+
+pp @store1
+
+@store2 = Store.second
+
+pp @store2
+
+@store1.update(name: 'Vancouver')
+
+pp @store1
