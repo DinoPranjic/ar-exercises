4885cef18364d48afcf695b0dd352dd80dca1335
DinoPranjic <dinopranjic92@gmail.com>
2022-06-28 01:40:47 +0000

    complete exercises 6 and 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store 
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Dino", last_name: "Pranjic", hourly_rate: 80)
+@store2.employees.create(first_name: "Billy", last_name: "Bob", hourly_rate: 20)
+@store2.employees.create(first_name: "Joe", last_name: "Blow", hourly_rate: 40)
+# pp @store1.employees
+# pp @store2.employees
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,29 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# store validations
+class Store < ActiveRecord::Base
+  validates :name, presence: true, :length => { :minimum => 3 }
+  validates :annual_revenue, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+  # BONUS VALIDATION
+  validate :mens_or_womens_apparel
+
+  def mens_or_womens_apparel
+    if mens_apparel == nil && womens_apparel == nil
+      errors.add(:mens_apparel, :womens_apparel, "must specify a type of apparel sold!")
+    end
+  end
+end
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, :numericality => { :only_integer => true, :greater_than_or_equal_to => 40, :less_than_or_equal_to => 200}
+  validates_associated :store
+end
+
+
+Store.create(name: '', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+puts Store.new.errors[:name].any?
